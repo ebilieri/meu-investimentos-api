68216dcad1600ee463dcda5f32dc13e9619358f5
Emerson Claudelino <48636325+ebilieri@users.noreply.github.com>
2021-05-31 14:08:34 -0300

    add swagger and configuration

---
+++ MeusInvestimentosApi/MeusInvestimentosApi/Startup.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace MeusInvestimentosApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.UseCamelCasing(true);
+                    options.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+                    options.SerializerSettings.DateParseHandling = DateParseHandling.None;
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter());
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Meus Invetimentos API",
+                    Version = "v1",
+                    Description = "Meus Investimentos Pessoais",
+                    TermsOfService = new Uri("https://github.com/ebilieri"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Emerson Bilieri",
+                        Email = "ebilieri@gmail.com",
+                        Url = new Uri("https://github.com/ebilieri"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://github.com/ebilieri"),
+                    }
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            services.AddMemoryCache();
+
+            services.Configure<ConfigApi>(Configuration.GetSection("Program"));
+            var apiConfig = Configuration.GetSection("Program").Get<ConfigApi>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meus Investimentos Pessoais v1");
+            });
+        }
+    }
+}
